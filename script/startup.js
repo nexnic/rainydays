f132ec2133929ae091fc481b4a76242162ac0578
Kent Erik Hole <kent.erik.hole@outlook.com>
2022-12-20 17:15:28 +0100

    start on api script

---
+++ script/startup.js
@@ -0,0 +1,13 @@
+// use strice mode on 
+    'use strict';
+
+// API URL 
+    const URL = 'https://frontendkenterik.no/Rainyday-API/wp-json/wc/v3';
+
+async function apird(){
+    try {
+        
+    } catch (error) {   
+        console.log(error);
+    }
+}
